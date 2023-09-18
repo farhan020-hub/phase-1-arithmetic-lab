50837e79175108eb0ac9227ddb5cf8722b301c9b
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-18 20:53:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+console.log(multiply); 
+
+
+const random = 1;
+Math.random();
+
+
+const num3 = 16;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+console.log(mod);
+
+const max = 20;
+
+
+
+ 
+ 
